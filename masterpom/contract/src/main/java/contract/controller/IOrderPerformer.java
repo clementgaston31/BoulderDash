7f3ad0afe052550fcee8c7fe5a58e2bbca9bb778
clementgaston31 <44499395+clementgaston31@users.noreply.github.com>
2019-05-31 18:35:21 +0200

    Delete IOrderPerformer.java

--- masterpom/contract/src/main/java/contract/controller/IOrderPerformer.java
+++
@@ -1,9 +0,0 @@
-package contract.controller;
-import java.io.IOException;
-
-import controller.UserOrder;
-
-public interface IOrderPerformer {
-
-	void orderPerform(UserOrder userOrder) throws IOException;
-}
